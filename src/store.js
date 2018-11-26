a9a8b1d9eb651bb023dd5ff64401ed3ca0d5567c
pkhafizov <pkhafizov@gmail.com>
2018-11-26 16:41:20 +0700

    fake messages added

--- src/store.js
+++ src/store.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    posts: []
   },
   mutations: {
-
+    updatePosts(state, posts) {
+      state.posts = posts;
+    }
   },
   actions: {
-
+    getPosts({
+      commit
+    }) {
+      return axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(function (response) {
+          commit('updatePosts', response.data)
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="home">
-    <Header nameModule="main"/>
+    <Header nameModule="main" />
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-4">Posts</h1>
+        <div class="container">
+          <div class="row pt-2">
+            <input class="btn btn-primary" type="button" value="get posts" @click="fetchPosts()">
+          </div>
+          <div class="row pt-2">
+            <table class="table table-light">
+              <tbody>
+                <tr v-for="post in posts" :key="post.id">
+                  <td>{{ post.title }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Header from '@/components/Header.vue'
+  import {
+    mapActions
+  } from "vuex";
+  // @ is an alias to /src
+  import Header from "@/components/Header.vue";
 
-export default {
-  name: 'home',
-  components: {
-    Header
-  }
-}
+  export default {
+    name: "home",
+    components: {
+      Header
+    },
+    computed: {
+      posts: function () {
+        return this.$store.state.posts;
+      }
+    },
+    methods: {
+      ...mapActions(["getPosts"]),
+      fetchPosts() {
+        this.getPosts();
+      }
+    }
+  };
 </script>
